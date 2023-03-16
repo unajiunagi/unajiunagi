d7b8a3dd5ec79548c519eb9290502161f12b71c0
t.teshima <teshima.yousuke@gmail.com>
2023-03-17 01:23:20 +0900

    next devで画面真っ白になる原因究明中

---
+++ src/pages/Index.tsx
@@ -0,0 +1,10 @@
+type Props = { };
+
+const Index = ({}: Props) => {
+  return (
+    // <MovieDescription bgimg = "img.jpg" title = 'title' description = '紹介文'/>
+    <h2>home</h2>
+  );
+};
+
+export default Index;
