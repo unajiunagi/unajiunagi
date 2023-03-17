6e2eead2cd9f508a479e26081b3f38cc6a0051b4
t.teshima <teshima.yousuke@gmail.com>
2023-03-17 13:15:51 +0900

    test

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,8 +4,7 @@ import { Box } from "@chakra-ui/react";
 export default function Home() {
   return (
     <>
-      {/* <MovieDescription bgimg="img.jpg" title="title" description="紹介文" /> */}
-      <h2>おはよう</h2>
+      <MovieDescription bgimg="img.jpg" title="title" description="紹介文" />
     </>
   );
 }
