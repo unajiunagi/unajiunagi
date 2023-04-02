8565d98515e591f7184c4233848d1448237a1e68
t.teshima <teshima.yousuke@gmail.com>
2023-04-02 20:33:09 +0900

    欠けていたLayout.tsxの復元

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import { Header } from "@/components/common/Header";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <div className="layout">{children}</div>
+    </>
+  );
+};
