7b24005a81d61aae254108094673bdff131a987e
t.teshima <teshima.yousuke@gmail.com>
2023-10-04 13:45:14 +0900

    apiの型エラーを解決

--- src/pages/api/supabase/getConnectedAccountId/[id].ts
+++ src/pages/api/supabase/getConnectedAccountId/[id].ts
@@ -6,9 +6,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') res.status(405);
 
   const { id } = req.query;
+  const connectedId = id as string;
   const supabaseServerClient = createPagesServerClient<Database>({ req, res });
 
-  const { data, error } = await supabaseServerClient.from('users').select('stripe_connected_id').eq('id', id).single();
+  const { data, error } = await supabaseServerClient.from('users').select('stripe_connected_id').eq('id', connectedId).single();
 
   if (error) return res.status(500).json({ message: error });
 
