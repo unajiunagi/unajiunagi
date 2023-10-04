2ff517b248d038064520d98132ac600ce4114633
t.teshima <teshima.yousuke@gmail.com>
2023-10-04 12:33:19 +0900

    決済機能の実装を中断

--- src/components/watchPage/CheckoutForm.stories.tsx
+++ src/components/watchPage/CheckoutForm.stories.tsx
@@ -1,14 +1,14 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { CheckoutForm } from "components/watchPage/CheckoutForm";
+// import type { Meta, StoryObj } from "@storybook/react";
+// import { CheckoutForm } from "components/watchPage/CheckoutForm";
 
-const meta: Meta<typeof CheckoutForm> = {
-  component: CheckoutForm,
-};
-export default meta;
+// const meta: Meta<typeof CheckoutForm> = {
+//   component: CheckoutForm,
+// };
+// export default meta;
 
-export const Default: StoryObj<typeof CheckoutForm> = {
-  args: {
+// export const Default: StoryObj<typeof CheckoutForm> = {
+//   args: {
 
-  },
-  name: "デフォルト",
-};
+//   },
+//   name: "デフォルト",
+// };
